8e41b3bca93a9c3dc062df90567d8765485e1980
evarku7 <alexp805@ufl.edu>
2017-01-18 01:55:56 -0500

    start ui design :)

--- textmeh/urls.py
+++ textmeh/urls.py
@@ -2,17 +2,20 @@ from django.conf.urls import url
 
 from . import views
 
-# Oficial Routes
+# Official Routes
 
 urlpatterns = [
 
     # Home Page
     url(r'^$', views.home_page, name='home_page'),
 
+    # Registration Page
     url(r'^registration$', views.registration, name='registration'),
 
+    # Chat Board Page
     url(r'^chat/(?P<username>\w+)/$', views.chat, name='chat'),
 
+    # Chat Messages Page
     url(r'^chat/(?P<username>\w+)/messages$', views.messages, name='messages'),
 
 
